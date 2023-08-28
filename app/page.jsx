ae95e17dc6b1c2fbc44f6368df35c5a3adec6928
Joseph López Henao <joseph.lopez2@soy.sena.edu.co>
2023-08-28 18:37:22 -0500

    Seccion Principal, Cambio De Informacion. seccion de nosotros puesta en una pagina aparte

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,6 @@ import Layout from "../app/layout";
 import Homeu from './components/pronosticos';
 import Meteorologicas from './components/variables';
 import QueEs from './components/que_es';
-import Nosotros from './components/sobre_nosotros';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -14,7 +13,6 @@ function Page() {
       <Homeu />
       <Meteorologicas />
       <QueEs />
-      <Nosotros />
       <Footer />
     </Layout>
   );
---
+++ app/pages/Nosotros/page.jsx
@@ -0,0 +1,15 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import Nosotros from "../../components/sobre_nosotros";
+
+const NosotrosPage = () => {
+    return (
+        <div>
+        <Header/>
+        <Nosotros/>
+        <Footer/>
+        </div>
+    );
+  };
+  
+  export default NosotrosPage;
