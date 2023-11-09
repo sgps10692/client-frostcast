1049989014c26f8fc25b2f5a71159e3939254ff5
Juan Pablo <jpsanabria37@soy.sena.edu.co>
2023-11-09 00:32:25 -0500

    cambios

--- app/dashboard/variables-meteorologicas/components/SelectLocation.jsx
+++ app/dashboard/variables-meteorologicas/components/SelectLocation.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Line, Bar } from "react-chartjs-2";
 import { DateTime } from "luxon"; // Importa DateTime de luxon
 import "chartjs-adapter-date-fns";
@@ -34,159 +34,19 @@ ChartJS.register(
 );
 
 export default function SelectLocation({ locations }) {
-  const [data, setData] = useState(null);
-  const [chartData, setChartData] = useState(null);
+  const [chartData, setChartData] = useState({});
 
   const fetchPredictionData = async (locationId) => {
-    try {
-      const response = await fetch(
-        `http://127.0.0.1:8000/api/ubicaciones/${locationId}/predicciones`
-      );
-      const responseData = await response.json();
-      setData(responseData);
-    } catch (error) {
-      console.error("Error fetching predictions:", error);
-    }
-  };
-
-  const fetchChartData = async (locationId) => {
-    try {
-      const response = await fetch(
-        `http://127.0.0.1:8000/api/locations/${locationId}/recent-predictions`
-      );
-      const responseData = await response.json();
-
-      const months = Object.keys(responseData.predictions_by_month);
-      const formattedData = {
-        labels: months,
-        datasets: [
-          {
-            label: "Probabilidad de Heladas",
-            data: months.map((month) => {
-              const predictions = responseData.predictions_by_month[month];
-              const lastPrediction = predictions[predictions.length - 1];
-              return lastPrediction.probability;
-            }),
-            fill: false,
-            borderColor: "rgba(75, 192, 192, 1)",
-          },
-        ],
-      };
-      setChartData(formattedData);
-    } catch (error) {
-      console.log("error", error);
-    }
+    fetch(`http://127.0.0.1:8000/api/ubicaciones/${locationId}/predicciones`)
+      .then((response) => response.json())
+      .then((data) => {
+        setChartData(data.chart_data);
+      });
   };
 
   const handleLocationChange = async (event) => {
     const selectedLocation = event.target.value;
     await fetchPredictionData(selectedLocation);
-    await fetchChartData(selectedLocation);
-  };
-
-  const dataExample = {
-    labels: [
-      "Ene",
-      "Feb",
-      "Mar",
-      "Abr",
-      "May",
-      "Jun",
-      "Jul",
-      "Ago",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dic",
-    ],
-    datasets: [
-      {
-        label: "Probabilidad de Heladas",
-        data: [15, 14, 3, 5, 0, 0, 0.5, 0, 2, 0, 2, 4],
-        fill: false,
-        borderColor: "rgba(75, 192, 192, 1)",
-      },
-    ],
-  };
-
-  const options = {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  };
-
-  const dataBar = {
-    labels: [
-      "Ene",
-      "Feb",
-      "Mar",
-      "Abr",
-      "May",
-      "Jun",
-      "Jul",
-      "Ago",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dic",
-    ],
-    datasets: [
-      {
-        data: [15, 14, 3, 5, 0, 0, 0.5, 0, 2, 0, 2, 4],
-        backgroundColor: "rgba(75, 192, 192, 0.6)",
-        borderColor: "rgba(75, 192, 192, 1)",
-        borderWidth: 1,
-      },
-      {
-        data: [
-          10, //ene
-          7, //feb
-          4, //mar
-          5, //abril
-          3, // may
-          0.5, //jun
-          0.5, //Jul
-          1, //ago
-          3, //sep
-          0.1, //oc
-          0, //nov
-          7.5, //dic
-        ],
-        backgroundColor: "rgba(75, 192, 192, 0.6)",
-        borderColor: "rgba(75, 192, 192, 1)",
-        borderWidth: 1,
-      },
-      {
-        data: [15, 6, 2.5, 5, 0.5, 0, 0.5, 0, 2, 0.5, 0.5, 13],
-        backgroundColor: "rgba(75, 192, 192, 0.6)",
-        borderColor: "rgba(75, 192, 192, 1)",
-        borderWidth: 1,
-      },
-    ],
-  };
-
-  const temperatureData = [
-    // Aquí debes proporcionar tus propios datos de temperatura
-    { date: "2023-09-01", temperature: 25 },
-    { date: "2023-09-02", temperature: 26 },
-    // Agrega más datos aquí
-  ];
-
-  const chartDataTemperatura = {
-    labels: temperatureData.map((entry) =>
-      DateTime.fromISO(entry.date).toJSDate()
-    ), // Convierte las fechas a objetos de JavaScript Date usando luxon
-    datasets: [
-      {
-        label: "Temperatura (°C)",
-        data: temperatureData.map((entry) => entry.temperature),
-        borderColor: "rgba(75, 192, 192, 1)",
-        borderWidth: 2,
-        fill: false,
-      },
-    ],
   };
 
   const optionsTemperatura = {
@@ -217,23 +77,20 @@ export default function SelectLocation({ locations }) {
     },
   };
 
-  const optionsBar = {
+  const chartOptions = {
     scales: {
-      y: {
-        beginAtZero: true,
-        title: {
-          display: true,
-          text: "Porcentaje (%)",
+      x: {
+        type: "time",
+        time: {
+          unit: "day",
         },
       },
-    },
-    plugins: {
-      legend: {
-        display: false,
-        position: "top",
+      y: {
+        beginAtZero: true,
       },
     },
   };
+
   const weatherData = {
     temperature: 25,
     humidity: 60,
@@ -262,38 +119,6 @@ export default function SelectLocation({ locations }) {
             </select>
           </div>
         </div>
-        <div>
-          {data ? (
-            <div className="text-center pt-4">
-              {data.prediccion_reciente ? (
-                <div>
-                  <p>
-                    Probabilidad de heladas:
-                    {data.prediccion_reciente.probability}%
-                  </p>
-                  <p>Fecha: {data.prediccion_reciente.date}</p>
-                </div>
-              ) : (
-                "No hay predicción de helada"
-              )}
-              {data.valores_meteorologicos ? (
-                <div>
-                  {data.valores_meteorologicos.map((valor) => (
-                    <p>
-                      {valor.parameter.name} : {valor.value}
-                    </p>
-                  ))}
-                </div>
-              ) : (
-                "No hay valores meteorologicos"
-              )}
-            </div>
-          ) : (
-            <p className="text-center pt-4">
-              No hay datos de predicción disponibles.
-            </p>
-          )}
-        </div>
       </div>
       <div className="grid grid-cols-1 gap-8 mt-5">
         <div className="p-8 bg-gray-100">
@@ -302,14 +127,42 @@ export default function SelectLocation({ locations }) {
       </div>
 
       <div className="grid grid-cols-2 gap-8 mt-5">
-        <div className="p-8 bg-gray-100">
-          <h2 className="text-2xl mb-4">Porcentaje de Ocurrencia de Heladas</h2>
-          <small> {data ? data.location.name : "Nueva Generación"}</small>
-          <br />
-          <div className="bg-white p-2 mt-5 rounded-md shadow-md">
-            <Line data={chartDataTemperatura} options={optionsTemperatura} />
-          </div>
-        </div>
+        {chartData.data &&
+          Object.keys(chartData.data).map((variableName) => (
+            <div className="p-8 bg-gray-100">
+              <div
+                className="bg-white p-2 m-5 rounded-md shadow-md"
+                key={variableName}
+              >
+                <h3>{variableName}</h3>
+                {Object.keys(chartData.data[variableName]).map(
+                  (parameterName) => (
+                    <div key={parameterName}>
+                      <h4>{parameterName}</h4>
+                      <Line
+                        data={{
+                          labels: chartData.data[variableName][
+                            parameterName
+                          ].map((entry) => entry.date),
+                          datasets: [
+                            {
+                              label: "Valor",
+                              data: chartData.data[variableName][
+                                parameterName
+                              ].map((entry) => entry.value),
+                              fill: false,
+                              borderColor: "rgb(75, 192, 192)",
+                            },
+                          ],
+                        }}
+                        options={chartOptions}
+                      />
+                    </div>
+                  )
+                )}
+              </div>
+            </div>
+          ))}
       </div>
     </>
   );
